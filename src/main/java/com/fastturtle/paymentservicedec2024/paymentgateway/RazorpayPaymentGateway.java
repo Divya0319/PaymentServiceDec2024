12510754d6acf7ed1949a0d2d2dd4e4ef36853b5
Divya Gupta <divygupta0319@gmail.com>
2024-12-07 18:04:36 +0530

    Added payment link generation code

--- src/main/java/com/fastturtle/paymentservicedec2024/paymentgateway/RazorpayPaymentGateway.java
+++ src/main/java/com/fastturtle/paymentservicedec2024/paymentgateway/RazorpayPaymentGateway.java
@@ -1,11 +1,54 @@
 package com.fastturtle.paymentservicedec2024.paymentgateway;
 
+import com.razorpay.PaymentLink;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.json.JSONObject;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
 
 @Component
 public class RazorpayPaymentGateway implements PaymentGateway {
+
+    @Autowired
+    private RazorpayClient razorpayClient;
+
     @Override
     public String getPaymentLink() {
-        return null;
+        try {
+
+            //RazorpayClient razorpay = new RazorpayClient("[YOUR_KEY_ID]", "[YOUR_KEY_SECRET]");
+            JSONObject paymentLinkRequest = new JSONObject();
+            paymentLinkRequest.put("amount",1000);
+            paymentLinkRequest.put("currency","INR");
+            paymentLinkRequest.put("accept_partial",true);
+            paymentLinkRequest.put("first_min_partial_amount",100);
+            paymentLinkRequest.put("expire_by",1691097057);
+            paymentLinkRequest.put("reference_id","TS1989");
+            paymentLinkRequest.put("description","Payment for policy no #23456");
+            JSONObject customer = new JSONObject();
+            customer.put("name","+919000090000");
+            customer.put("contact","Gaurav Kumar");
+            customer.put("email","gaurav.kumar@example.com");
+            paymentLinkRequest.put("customer",customer);
+            JSONObject notify = new JSONObject();
+            notify.put("sms",true);
+            notify.put("email",true);
+            paymentLinkRequest.put("notify",notify);
+            paymentLinkRequest.put("reminder_enable",true);
+            JSONObject notes = new JSONObject();
+            notes.put("policy_name","Jeevan Bima");
+            paymentLinkRequest.put("notes",notes);
+            paymentLinkRequest.put("callback_url","https://example-callback-url.com/");
+            paymentLinkRequest.put("callback_method","get");
+
+
+            PaymentLink payment = razorpayClient.paymentLink.create(paymentLinkRequest);
+            return payment.get("short_url").toString();
+        } catch (RazorpayException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
