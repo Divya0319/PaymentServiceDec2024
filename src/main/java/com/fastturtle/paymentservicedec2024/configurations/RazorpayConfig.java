e906357eef8c2808e10622563d0245243f4018f3
Divya Gupta <divygupta0319@gmail.com>
2024-12-07 18:04:15 +0530

    Added Razorpay config class and access key and secret key in properties file

--- src/main/java/com/fastturtle/paymentservicedec2024/configurations/RazorpayConfig.java
+++ src/main/java/com/fastturtle/paymentservicedec2024/configurations/RazorpayConfig.java
@@ -1,2 +1,26 @@
-package com.fastturtle.paymentservicedec2024.configurations;public class RazorpayConfig {
+package com.fastturtle.paymentservicedec2024.configurations;
+
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RazorpayConfig {
+
+    @Value("${razorpay.key}")
+    private String razorPayKey;
+
+    @Value("${razorpay.secret}")
+    private String razorPaySecret;
+
+    @Bean
+    public RazorpayClient getRazorPayClient() {
+        try {
+            return new RazorpayClient(razorPayKey, razorPaySecret);
+        } catch (RazorpayException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
