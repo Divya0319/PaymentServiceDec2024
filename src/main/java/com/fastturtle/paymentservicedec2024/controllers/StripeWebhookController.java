b306bcc6243e391ac56165af4dffeb65be87ace2
Divya Gupta <divygupta0319@gmail.com>
2024-12-10 12:37:40 +0530

    Created webhook api in Stripe, also enabled webhooks in Stripe dashboard, tunneling our url through ngrok now

---
+++ src/main/java/com/fastturtle/paymentservicedec2024/controllers/StripeWebhookController.java
@@ -0,0 +1,16 @@
+package com.fastturtle.paymentservicedec2024.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stripeWebhook")
+public class StripeWebhookController {
+
+    @PostMapping
+    public void respondToEvent(@RequestBody String event) {
+        System.out.println(event);
+    }
+}
